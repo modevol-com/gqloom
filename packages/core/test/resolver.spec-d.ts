fea33fd0092cb189cb97f396f4011daad76114d9
xcfox <xcfox@qq.com>
2024-04-16 19:34:29 +0800

    ⚙️ refactor(core): move resolver.spec-d.ts

--- packages/core/test/resolver.spec-d.ts
+++ packages/core/test/resolver.spec-d.ts
@@ -6,8 +6,8 @@ import {
   GraphQLString,
 } from "graphql"
 import { describe, expectTypeOf, it } from "vitest"
-import { silk } from "./silk"
-import { loom } from "./resolver"
+import { silk } from "../src/resolver/silk"
+import { loom } from "../src/resolver/resolver"
 
 const { resolver, query, mutation, field, subscription } = loom
 
