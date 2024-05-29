cad4de6c84f1be4e08509e0adfaaef26e281037f
xcfox <xcfox@qq.com>
2024-05-29 14:30:18 +0800

    🧪 test(MikroSilk): handle non null and array

--- packages/mikro-orm/test/schema.spec.ts
+++ packages/mikro-orm/test/schema.spec.ts
@@ -1,29 +1,30 @@
 import { describe, it, expect } from "vitest"
-import { printType } from "graphql"
+import { type GraphQLObjectType, printType } from "graphql"
 import { mikroSilk } from "../src"
 import { EntitySchema } from "@mikro-orm/core"
 
 const nullable = true
 
 describe("MikroSilk", () => {
-  it("should handle object", () => {
-    interface IBook {
-      title: string
-      isPublished: boolean
-      price: number
-      tags: string[]
-    }
-    const BookSchema = new EntitySchema<IBook>({
-      name: "Book",
-      properties: {
-        title: { type: "string" },
-        isPublished: { type: "boolean" },
-        price: { type: "number", nullable },
-        tags: { type: "string[]", array: true },
-      },
-    })
+  interface IBook {
+    title: string
+    isPublished: boolean
+    price: number
+    tags: string[]
+  }
+  const BookSchema = new EntitySchema<IBook>({
+    name: "Book",
+    properties: {
+      title: { type: "string" },
+      isPublished: { type: "boolean" },
+      price: { type: "number", nullable },
+      tags: { type: "string[]", array: true },
+    },
+  })
 
-    const gqlType = mikroSilk(BookSchema).getGraphQLType()
+  const gqlType = mikroSilk(BookSchema).getGraphQLType() as GraphQLObjectType
+
+  it("should handle object", () => {
     expect(printType(gqlType)).toMatchInlineSnapshot(`
       "type Book {
         title: String!
@@ -32,5 +33,19 @@ describe("MikroSilk", () => {
         tags: [String!]!
       }"
     `)
+  })
+
+  it("should handle non null", () => {
+    expect(gqlType.getFields()["title"].type).toMatchInlineSnapshot(`"String!"`)
+  })
+
+  it("should handle nullable", () => {
+    expect(gqlType.getFields()["price"].type).toMatchInlineSnapshot(`"Float"`)
+  })
+
+  it("should handle array", () => {
+    expect(gqlType.getFields()["tags"].type).toMatchInlineSnapshot(
+      `"[String!]!"`
+    )
   })
 })
