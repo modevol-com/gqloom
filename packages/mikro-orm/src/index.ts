7cafd3fee1d5b4686c2388fb9c94439006572d7e
xcfox <xcfox@qq.com>
2024-05-29 11:05:56 +0800

    ✨ feat(MikroSilk): handle object

---
+++ packages/mikro-orm/src/index.ts
@@ -0,0 +1,84 @@
+import { mapValue, type GraphQLSilk } from "@gqloom/core"
+import {
+  type EntityProperty,
+  type EntitySchema,
+  type RequiredEntityData,
+} from "@mikro-orm/core"
+import {
+  type GraphQLFieldConfig,
+  GraphQLObjectType,
+  type GraphQLOutputType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} from "graphql"
+
+export class MikroSilk<TEntity>
+  implements GraphQLSilk<TEntity, RequiredEntityData<TEntity>>
+{
+  _types?: { output: TEntity; input: RequiredEntityData<TEntity> }
+
+  getGraphQLType() {
+    return new GraphQLObjectType({
+      name: this.schema.meta.className,
+      fields: mapValue(this.schema.meta.properties, MikroSilk.propertyToField),
+    })
+  }
+
+  constructor(public readonly schema: EntitySchema<TEntity, any>) {}
+
+  static propertyToField(
+    property: EntityProperty
+  ): GraphQLFieldConfig<any, any> {
+    const type = MikroSilk.propertyGraphQLType(property)
+    return { type }
+  }
+
+  static propertyGraphQLType(property: EntityProperty): GraphQLOutputType {
+    let gqlType = MikroSilk.propertyGraphQLTypeInner(property)
+    gqlType = list(gqlType)
+    gqlType = nonNull(gqlType)
+    return gqlType
+
+    function list(gqlType: GraphQLOutputType) {
+      if (property.array) return new GraphQLList(gqlType)
+      return gqlType
+    }
+
+    function nonNull(gqlType: GraphQLOutputType) {
+      if (!property.nullable) return new GraphQLNonNull(gqlType)
+      return gqlType
+    }
+  }
+
+  static propertyGraphQLTypeInner(property: EntityProperty): GraphQLOutputType {
+    switch (property.type) {
+      case "string":
+        return GraphQLString
+      case "double":
+      case "number":
+      case "decimal":
+      case "float":
+        return GraphQLFloat
+      case "smallint":
+      case "tinyint":
+      case "bigint":
+      case "mediumint":
+      case "integer":
+        return GraphQLInt
+      case "boolean":
+        return GraphQLBoolean
+      default:
+        return GraphQLString
+    }
+  }
+}
+
+export function mikroSilk<TEntity>(
+  schema: EntitySchema<TEntity, any>
+): MikroSilk<TEntity> {
+  return new MikroSilk(schema)
+}
---
+++ packages/mikro-orm/test/schema.spec.ts
@@ -0,0 +1,36 @@
+import { describe, it, expect } from "vitest"
+import { printType } from "graphql"
+import { mikroSilk } from "../src"
+import { EntitySchema } from "@mikro-orm/core"
+
+const nullable = true
+
+describe("MikroSilk", () => {
+  it("should handle object", () => {
+    interface IBook {
+      title: string
+      isPublished: boolean
+      price: number
+      tags: string[]
+    }
+    const BookSchema = new EntitySchema<IBook>({
+      name: "Book",
+      properties: {
+        title: { type: "string" },
+        isPublished: { type: "boolean" },
+        price: { type: "number", nullable },
+        tags: { type: "string", array: true },
+      },
+    })
+
+    const gqlType = mikroSilk(BookSchema).getGraphQLType()
+    expect(printType(gqlType)).toMatchInlineSnapshot(`
+      "type Book {
+        title: String!
+        isPublished: Boolean!
+        price: Float
+        tags: [String]!
+      }"
+    `)
+  })
+})
