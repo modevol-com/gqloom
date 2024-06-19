93b654d357c12f24dd53dcb309532a22acc485d6
xcfox <xcfox@qq.com>
2024-06-19 16:45:44 +0800

    🧪 test(valibot): handle non null

--- packages/valibot/test/schema.spec.ts
+++ packages/valibot/test/schema.spec.ts
@@ -5,10 +5,25 @@ import {
   GraphQLFloat,
   GraphQLID,
   GraphQLInt,
+  type GraphQLObjectType,
+  GraphQLScalarType,
   GraphQLString,
+  GraphQLNonNull,
 } from "graphql"
 import * as v from "valibot"
-import { getGraphQLType } from "@gqloom/core"
+import { type GQLoomExtensions, getGraphQLType } from "@gqloom/core"
+import { asField } from "../src/metadata"
+
+declare module "graphql" {
+  export interface GraphQLObjectTypeExtensions extends GQLoomExtensions {}
+
+  export interface GraphQLFieldExtensions<_TSource, _TContext, _TArgs = any>
+    extends GQLoomExtensions {}
+}
+
+const GraphQLDate = new GraphQLScalarType<Date, string>({
+  name: "Date",
+})
 
 describe("valibot", () => {
   it("should handle scalar", () => {
@@ -29,6 +44,52 @@ describe("valibot", () => {
       GraphQLID
     )
     expect(getGraphQLType(valibotSilk(v.pipe(v.string(), v.email())))).toEqual(
+      GraphQLString
+    )
+  })
+
+  it.skip("should keep default value in extensions", () => {
+    const objectType = v.object({
+      foo: v.optional(v.string(), "foo"),
+    })
+
+    const objectGqlType = getGraphQLType(
+      valibotSilk(objectType)
+    ) as GraphQLObjectType
+
+    const extensions = objectGqlType.getFields().foo.extensions
+
+    expect(extensions.gqloom?.defaultValue).toEqual(expect.any(Function))
+    expect(extensions.gqloom?.defaultValue?.()).toEqual("foo")
+  })
+
+  it("should handle custom type", () => {
+    expect(
+      getGraphQLType(
+        valibotSilk(v.pipe(v.date(), asField({ type: GraphQLDate })))
+      )
+    ).toEqual(GraphQLDate)
+  })
+
+  it("should handle non null", () => {
+    expect(getGraphQLType(valibotSilk(v.string()))).toEqual(GraphQLString)
+    expect(getGraphQLType(valibotSilk(v.nonNullable(v.string())))).toEqual(
+      new GraphQLNonNull(GraphQLString)
+    )
+    expect(getGraphQLType(valibotSilk(v.nonOptional(v.string())))).toEqual(
+      new GraphQLNonNull(GraphQLString)
+    )
+    expect(getGraphQLType(valibotSilk(v.nonNullish(v.string())))).toEqual(
+      new GraphQLNonNull(GraphQLString)
+    )
+
+    expect(getGraphQLType(valibotSilk(v.nullable(v.string())))).toEqual(
+      GraphQLString
+    )
+    expect(getGraphQLType(valibotSilk(v.optional(v.string())))).toEqual(
+      GraphQLString
+    )
+    expect(getGraphQLType(valibotSilk(v.nullish(v.string())))).toEqual(
       GraphQLString
     )
   })
