0ad6378340c57fae09de950911d4be8ff75226d2
xcfox <xcfox@qq.com>
2024-06-21 23:11:26 +0800

    🧪 test(InferPropertyType): add test

---
+++ packages/core/src/utils/types.spec-d.ts
@@ -0,0 +1,30 @@
+import { describe, expectTypeOf, it } from "vitest"
+import { type InferPropertyType } from "./types"
+
+interface A {
+  a?: { b?: { c: string } }
+}
+
+interface B {
+  b: { c: string }
+  d: { c: string }
+}
+
+describe("InferPropertyType", () => {
+  it("should infer property", () => {
+    type C = InferPropertyType<A, "a">
+    expectTypeOf<C>().toEqualTypeOf<{ b?: { c: string } } | undefined>()
+  })
+
+  it("should infer nested property", () => {
+    type C = InferPropertyType<A, "a.b">
+    expectTypeOf<C>().toEqualTypeOf<{ c: string } | undefined>()
+  })
+
+  it("should infer union property", () => {
+    type C = InferPropertyType<B, "b" | "d">
+    expectTypeOf<C>().toEqualTypeOf<{
+      c: string
+    }>()
+  })
+})
