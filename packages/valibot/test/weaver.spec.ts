37424bdb6cc1f927f3ca5d1391e3bc58e09ade24
xcfox <xcfox@qq.com>
2024-06-24 20:23:49 +0800

    ⚙️ refactor(zod | yup): remove weaver test

--- packages/valibot/test/weaver.spec.ts
+++
@@ -1,5 +0,0 @@
-import { describe, it } from "vitest"
-
-describe.todo("Weaver with valibotSilk", () => {
-  it.todo("should use preset GraphQLType")
-})
--- packages/zod/test/weaver.spec.ts
+++
@@ -1,5 +0,0 @@
-import { describe, it } from "vitest"
-
-describe.todo("Weaver with ZodSilk", () => {
-  it.todo("should use preset GraphQLType")
-})
