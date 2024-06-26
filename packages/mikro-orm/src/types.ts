62863e15a1aab4e52add8bc6347a51d88c5481a2
xcfox <xcfox@qq.com>
2024-06-27 02:34:25 +0800

    ⚙️ refactor(mikro-orm): remove useless type

--- packages/mikro-orm/src/types.ts
+++ packages/mikro-orm/src/types.ts
@@ -1,6 +1,5 @@
-import { type WeaverConfig, type SYMBOLS, type GraphQLSilk } from "@gqloom/core"
+import { type WeaverConfig, type SYMBOLS } from "@gqloom/core"
 import {
-  type RequiredEntityData,
   type EntityProperty,
   type EntitySchema,
   type PropertyOptions,
@@ -25,6 +24,3 @@ export interface MikroWeaverConfig
     MikroWeaverConfigOptions {
   [SYMBOLS.WEAVER_CONFIG]: "gqloom.mikro-orm"
 }
-
-export type EntitySchemaSilk<TSchema extends EntitySchema> = TSchema &
-  GraphQLSilk<InferEntity<TSchema>, RequiredEntityData<InferEntity<TSchema>>>
