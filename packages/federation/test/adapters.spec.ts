c644659e1af3bf929df547f204e99520bae501d2
⛵ <809067559@qq.com>
2024-07-13 13:57:35 +0800

    🧪 test(federation): Mercurius

--- packages/federation/test/adapters.spec.ts
+++ packages/federation/test/adapters.spec.ts
@@ -1,7 +1,11 @@
+import { describe, expect, it } from "vitest"
 import { ApolloServer } from "@apollo/server"
 import gql from "graphql-tag"
 import { buildSubgraphSchema } from "@apollo/subgraph"
-import { describe, expect, it } from "vitest"
+import { createYoga } from "graphql-yoga"
+import Fastify from "fastify"
+import { mercuriusFederationPlugin } from "@mercuriusjs/federation"
+import { createMercuriusTestClient } from "mercurius-integration-testing"
 
 const typeDefs = gql`
   extend schema
@@ -16,26 +20,26 @@ const typeDefs = gql`
 
   type User @key(fields: "id") {
     id: ID!
-    username: String
+    name: String
   }
 `
 
 const resolvers = {
   Query: {
     me() {
-      return { id: "1", username: "@ava" }
+      return { id: "1", name: "@ava" }
     },
   },
   User: {
     __resolveReference({ id }: { id: string }) {
-      return { id, username: "@ava" }
+      return { id, name: "@ava" }
     },
   },
 }
-const schema = buildSubgraphSchema({ typeDefs, resolvers })
-const server = new ApolloServer({ schema })
 
 describe("Apollo", () => {
+  const schema = buildSubgraphSchema({ typeDefs, resolvers })
+  const server = new ApolloServer({ schema })
   it("should build schema", () => {
     expect(schema).toBeDefined()
   })
@@ -46,7 +50,7 @@ describe("Apollo", () => {
         query {
           me {
             id
-            username
+            name
           }
         }
       `,
@@ -54,7 +58,7 @@ describe("Apollo", () => {
 
     if (reponse.body.kind !== "single") throw new Error("unexpected")
     expect(reponse.body.singleResult.data).toMatchObject({
-      me: { id: "1", username: "@ava" },
+      me: { id: "1", name: "@ava" },
     })
   })
 
@@ -65,7 +69,7 @@ describe("Apollo", () => {
           _entities(representations: $representations) {
             ... on User {
               id
-              username
+              name
             }
           }
         }
@@ -81,9 +85,139 @@ describe("Apollo", () => {
     if (reponse.body.kind !== "single") throw new Error("unexpected")
     expect(reponse.body.singleResult.data).toMatchObject({
       _entities: [
-        { id: "1", username: "@ava" },
-        { id: "2", username: "@ava" },
+        { id: "1", name: "@ava" },
+        { id: "2", name: "@ava" },
       ],
+    })
+  })
+})
+
+describe("Yoga", () => {
+  const schema = buildSubgraphSchema({ typeDefs, resolvers })
+  const yoga = createYoga({ schema })
+
+  it("should build schema", () => {
+    expect(schema).toBeDefined()
+  })
+  it("should execute query", async () => {
+    const response = await yoga.fetch("http://localhost/graphql", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          query {
+            me {
+              id
+              name
+            }
+          }
+        `,
+      }),
+    })
+
+    if (response.status !== 200) throw new Error("unexpected")
+    const json = await response.json()
+    expect(json).toMatchObject({
+      data: { me: { id: "1", name: "@ava" } },
+    })
+  })
+
+  it("should execute query for entities", async () => {
+    const response = await yoga.fetch("http://localhost/graphql", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          query ($representations: [_Any!]!) {
+            _entities(representations: $representations) {
+              ... on User {
+                id
+                name
+              }
+            }
+          }
+        `,
+        variables: {
+          representations: [
+            { __typename: "User", id: "1" },
+            { __typename: "User", id: "2" },
+          ],
+        },
+      }),
+    })
+
+    if (response.status !== 200) throw new Error("unexpected")
+    const json = await response.json()
+    expect(json).toMatchObject({
+      data: {
+        _entities: [
+          { id: "1", name: "@ava" },
+          { id: "2", name: "@ava" },
+        ],
+      },
+    })
+  })
+})
+
+describe("Mercurius", () => {
+  const app = Fastify()
+  app.register(mercuriusFederationPlugin, {
+    schema: typeDefs,
+    resolvers,
+  })
+  const client = createMercuriusTestClient(app)
+
+  it("should build schema", () => {
+    expect(app.graphql.schema).toBeDefined()
+  })
+
+  it("should execute query", async () => {
+    const response = await client.query(/* GraphQL */ `
+      query {
+        me {
+          id
+          name
+        }
+      }
+    `)
+
+    expect(response).toMatchObject({
+      data: { me: { id: "1", name: "@ava" } },
+    })
+  })
+
+  it("should execute query for entities", async () => {
+    const response = await client.query(
+      /* GraphQL */ `
+        query ($representations: [_Any!]!) {
+          _entities(representations: $representations) {
+            ... on User {
+              id
+              name
+            }
+          }
+        }
+      `,
+      {
+        variables: {
+          representations: [
+            { __typename: "User", id: "1" },
+            { __typename: "User", id: "2" },
+          ],
+        },
+      }
+    )
+    expect(response).toMatchObject({
+      data: {
+        _entities: [
+          { id: "1", name: "@ava" },
+          { id: "2", name: "@ava" },
+        ],
+      },
     })
   })
 })
