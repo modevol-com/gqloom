f2e68d7ecb7a53f86cbd8dbd9b6d0b7acfc83846
⛵ <809067559@qq.com>
2024-07-18 13:31:43 +0800

    🧪 test(valibot): asObjectType

--- packages/valibot/test/metadata.spec.ts
+++ packages/valibot/test/metadata.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest"
-import { asInputArgs } from "../src"
+import { asInputArgs, asObjectType, ValibotMetadataCollector } from "../src"
+import * as v from "valibot"
 
 describe("asInputArgs", () => {
   it("should return asInputArgs", () => {
@@ -13,5 +14,32 @@ describe("asInputArgs", () => {
     const a1 = asInputArgs()
     const a2 = asInputArgs()
     expect(a1.config.name).not.toEqual(a2.config.name)
+  })
+})
+
+describe("asObjectType", () => {
+  it("should return asObjectType", () => {
+    const result = asObjectType({})
+    expect(result).toMatchObject({
+      type: "gqloom.asObjectType",
+    })
+  })
+
+  it("should get GraphQL Object type config", () => {
+    const o = v.pipe(
+      v.object({
+        id: v.string(),
+        name: v.string(),
+      }),
+      asObjectType({
+        name: "User",
+        extensions: { some: "value" },
+      })
+    )
+
+    expect(ValibotMetadataCollector.getObjectConfig(o)).toMatchObject({
+      name: "User",
+      extensions: { some: "value" },
+    })
   })
 })
