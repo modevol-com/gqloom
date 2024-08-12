671d86b1eee1bbb67c568f828545c630af808412
⛵ <809067559@qq.com>
2024-08-12 23:20:56 +0800

    🧪 test(federation): Federated server

--- packages/federation/test/schema.spec.ts
+++ packages/federation/test/schema.spec.ts
@@ -9,6 +9,8 @@ import {
   printSchema,
 } from "graphql"
 import { printSubgraphSchema } from "@apollo/subgraph"
+import { ApolloServer } from "@apollo/server"
+import { ApolloServerPluginInlineTraceDisabled } from "@apollo/server/plugin/disabled"
 
 describe("FederatedSchemaWeaver", () => {
   interface IUser {
@@ -95,5 +97,87 @@ describe("FederatedSchemaWeaver", () => {
         sdl: String
       }"
     `)
+  })
+
+  describe("Federated GraphQL Server", () => {
+    const queries = {
+      me: /* GraphQL */ `
+        query me {
+          me {
+            id
+            name
+          }
+        }
+      `,
+      entities: /* GraphQL */ `
+        query entities($representations: [_Any!]!) {
+          _entities(representations: $representations) {
+            ... on User {
+              id
+              name
+            }
+          }
+        }
+      `,
+      subgraphSchema: /* GraphQL */ `
+        query subgraphSchema {
+          _service {
+            sdl
+          }
+        }
+      `,
+    }
+    const server = new ApolloServer({
+      schema,
+      plugins: [ApolloServerPluginInlineTraceDisabled()],
+    })
+    it("should execute normal query", async () => {
+      const response = await server.executeOperation({
+        query: queries.me,
+      })
+
+      if (response.body.kind !== "single") throw new Error("unexpected")
+
+      expect(response.body.singleResult.data).toMatchObject({
+        me: { id: "1", name: "@ava" },
+      })
+    })
+
+    it("should execute query for entities", async () => {
+      const response = await server.executeOperation({
+        query: queries.entities,
+        variables: {
+          representations: [
+            { __typename: "User", id: "1" },
+            { __typename: "User", id: "2" },
+          ],
+        },
+      })
+
+      if (response.body.kind !== "single") throw new Error("unexpected")
+      expect(response.body.singleResult.data).toMatchObject({
+        _entities: [
+          { id: "1", name: "@ava" },
+          { id: "2", name: "@ava" },
+        ],
+      })
+    })
+
+    it("should introspect Subgraph Schema", async () => {
+      const response = await server.executeOperation({
+        query: queries.subgraphSchema,
+      })
+
+      if (response.body.kind !== "single") throw new Error("unexpected")
+      const sdlFromResponse = (
+        response.body.singleResult.data as unknown as {
+          _service: { sdl: string }
+        }
+      )._service.sdl
+      expect(sdlFromResponse).toMatch("type Query")
+      expect(sdlFromResponse).toEqual(
+        printSubgraphSchema(lexicographicSortSchema(schema))
+      )
+    })
   })
 })
