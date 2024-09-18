ded4584b3fff11bb90106ccfecf1a69eb75dde16
⛵ <809067559@qq.com>
2024-09-18 22:31:29 +0800

    ⚙️ refactor(website): update config to include title, social links, and theme options for GQLoom

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -8,6 +8,7 @@ import {
 export default defineConfig({
   // 文档根目录
   root: "docs",
+  title: "GQLoom",
   lang: "en",
   logo: { dark: "/gqloom-name.svg", light: "/gqloom-name-light.svg" },
   icon: "/gqloom.svg",
@@ -27,6 +28,32 @@ export default defineConfig({
       description: "GraphQL 纺织机",
     },
   ],
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/modevol-com/gqloom",
+      },
+    ],
+    // 开启 View Transition 过渡
+    enableContentAnimation: true,
+    enableAppearanceAnimation: true,
+    locales: [
+      {
+        label: "English",
+        description: "GraphQL Loom",
+        lang: "en",
+        outlineTitle: "ON THIS Page",
+      },
+      {
+        label: "简体中文",
+        description: "GraphQL 织布机",
+        lang: "zh",
+        outlineTitle: "目录 ",
+      },
+    ],
+  },
   plugins: [
     pluginShiki({
       transformers: [createTransformerDiff(), createTransformerHighlight()],
