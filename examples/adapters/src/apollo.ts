9156c905ba2815d27f267b4039805c6bb5c68929
⛵ <809067559@qq.com>
2024-09-21 20:36:23 +0800

    📄 docs(guide): apollo

---
+++ examples/adapters/src/apollo.ts
@@ -0,0 +1,14 @@
+import { weave } from "@gqloom/core"
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+
+import { HelloResolver } from "./resolvers"
+
+const schema = weave(HelloResolver)
+const server = new ApolloServer({ schema })
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.info(`🚀  Server ready at: ${url}`)
+})
