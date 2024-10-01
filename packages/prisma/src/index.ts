58b9b29604758562676ba913784b48e0ec39a773
⛵ <809067559@qq.com>
2024-10-01 23:16:22 +0800

    ✨ feat(PrismaWeaver): getGraphQLField

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,6 +1,23 @@
-import { type GraphQLSilk, SYMBOLS, silk } from "@gqloom/core"
+import {
+  type GraphQLSilk,
+  SYMBOLS,
+  silk,
+  notNullish,
+  weaverContext,
+  type WeaverConfig,
+} from "@gqloom/core"
 import { type DMMF } from "@prisma/generator-helper"
-import { GraphQLObjectType } from "graphql"
+import {
+  type GraphQLFieldConfig,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLNonNull,
+  type GraphQLOutputType,
+} from "graphql"
 
 export class PrismaWeaver {
   static unravel<TModal>(model: DMMF.Model): PrismaModelSilk<TModal> {
@@ -16,15 +33,89 @@ export class PrismaWeaver {
     }
   }
 
-  static getGraphQLTypeByModel(model: DMMF.Model): GraphQLObjectType {
-    return new GraphQLObjectType({
-      name: model.name,
-      fields: () => ({}),
-    })
+  static getGraphQLTypeByModel(model: DMMF.Model) {
+    const existing = weaverContext.getNamedType(model.name)
+    if (existing != null) return new GraphQLNonNull(existing)
+
+    return new GraphQLNonNull(
+      weaverContext.memoNamedType(
+        new GraphQLObjectType({
+          name: model.name,
+          fields: () =>
+            Object.fromEntries(
+              model.fields
+                .map((field) => {
+                  const fieldConfig = PrismaWeaver.getGraphQLField(field)
+                  return fieldConfig
+                    ? ([field.name, fieldConfig] as [
+                        string,
+                        GraphQLFieldConfig<any, any>,
+                      ])
+                    : null
+                })
+                .filter(notNullish)
+            ),
+        })
+      )
+    )
+  }
+
+  static getGraphQLField(
+    field: DMMF.Field
+  ): GraphQLFieldConfig<any, any> | undefined {
+    if (field.kind !== "scalar") return
+
+    const description = field.documentation
+    const unwrappedType = PrismaWeaver.getGraphQLTypeByField(field)
+
+    if (!unwrappedType) return
+
+    const type = field.isRequired
+      ? new GraphQLNonNull(unwrappedType)
+      : unwrappedType
+
+    return { type, description }
+  }
+
+  static getGraphQLTypeByField(
+    field: DMMF.Field
+  ): GraphQLOutputType | undefined {
+    const config = weaverContext.getConfig<PrismaWeaverConfig>("gqloom.prisma")
+
+    if (field.kind !== "scalar") return
+
+    const presetType = config?.presetGraphQLType?.(field)
+    if (presetType) return presetType
+    if (field.isId) return GraphQLID
+    switch (field.type) {
+      case "BigInt":
+      case "Int":
+        return GraphQLInt
+      case "Decimal":
+      case "Float":
+        return GraphQLFloat
+      case "Boolean":
+        return GraphQLBoolean
+      case "DateTime":
+      case "String":
+        return GraphQLString
+      default:
+        throw new Error(`Unsupported scalar type: ${field.type}`)
+    }
   }
 }
 
 export interface PrismaModelSilk<TModel> extends GraphQLSilk<TModel> {
   nullable(): GraphQLSilk<TModel | null>
   list(): GraphQLSilk<TModel[]>
+}
+
+export interface PrismaWeaverConfigOptions {
+  presetGraphQLType?: (field: DMMF.Field) => GraphQLOutputType | undefined
+}
+
+export interface PrismaWeaverConfig
+  extends WeaverConfig,
+    PrismaWeaverConfigOptions {
+  [SYMBOLS.WEAVER_CONFIG]: "gqloom.prisma"
 }
