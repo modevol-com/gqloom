b557a3531518e91a78232f947951588c1c2d591b
⛵ <809067559@qq.com>
2024-10-04 19:55:59 +0800

    ⚙️ refactor(example-prisma): remove generated

--- examples/prisma/src/generated/gqloom/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { PrismaModelSilk } from "@gqloom/prisma"
-import type {
-  User as IUser,
-  Post as IPost,
-} from "/Users/xcfox/Documents/code/github/gqloom/node_modules/.pnpm/@prisma+client@5.20.0_prisma@5.20.0/node_modules/@prisma/client"
-
-export const User: PrismaModelSilk<IUser>
-export const Post: PrismaModelSilk<IPost>
--- examples/prisma/src/generated/gqloom/index.js
+++
@@ -1,7 +0,0 @@
-import { PrismaWeaver } from "@gqloom/prisma"
-import datamodel from "./datamodel.json"
-
-const User = PrismaWeaver.unravel(datamodel.models.User)
-const Post = PrismaWeaver.unravel(datamodel.models.Post)
-
-export { User, Post }
