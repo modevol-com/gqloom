fdceace30134e37e55161a4a0cdbb800677eacbe
⛵ <809067559@qq.com>
2024-10-04 21:49:22 +0800

    ✨ feat(example-prisma): printSchema

--- examples/prisma/src/index.ts
+++ examples/prisma/src/index.ts
@@ -3,7 +3,10 @@ import * as v from "valibot"
 import { createServer } from "node:http"
 import { createYoga } from "graphql-yoga"
 import * as g from "./generated/gqloom"
-import { PrismaClient, type Prisma } from "@prisma/client"
+import { printSchema } from "graphql"
+import * as fs from "fs"
+import * as path from "path"
+// import { PrismaClient, type Prisma } from "@prisma/client"
 
 const HelloResolver = resolver({
   hello: query(v.string(), () => "Hello, World"),
@@ -11,10 +14,11 @@ const HelloResolver = resolver({
   user: query(g.User.nullable(), () => null),
 })
 
-export const schema = weave(HelloResolver)
+const schema = weave(HelloResolver)
+fs.writeFileSync(path.join(__dirname, "../schema.graphql"), printSchema(schema))
 
 const yoga = createYoga({ schema })
-
-createServer(yoga).listen(4000, () => {
+const server = createServer(yoga)
+server.listen(4000, () => {
   console.info("Server is running on http://localhost:4000/graphql")
 })
