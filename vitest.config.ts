bb97a2874c994fa1a766a25f4233732df9c34a09
⛵ <809067559@qq.com>
2024-10-18 22:43:06 +0800

    ⚙️ refactor(packages): update build and tsconfig paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,17 @@
 import { defineConfig } from "vitest/config"
 
-export default defineConfig({})
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: [
+        "**/examples/**/*",
+        "**/*.spec-d.ts",
+        "**/draft/**",
+        "website/**",
+        "vitest.workspace.ts",
+        "**/generated/*.ts",
+        "**/bin/*",
+      ],
+    },
+  },
+})
