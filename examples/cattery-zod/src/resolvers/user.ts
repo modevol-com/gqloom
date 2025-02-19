d4b92cdaa39e7965443ccb488e28f9af31e3d176
⛵ <809067559@qq.com>
2025-02-20 03:51:06 +0800

    feat(cattery-zod): add simple login functionality and current user query
    - Add `mine` query to return current user information
    - Implement basic login functionality using phone number
    - Update user resolver to include new query
    - Add error handling for unauthorized access

--- examples/cattery-zod/src/resolvers/user.ts
+++ examples/cattery-zod/src/resolvers/user.ts
@@ -1,9 +1,12 @@
 import { mutation, query, resolver } from "@gqloom/core"
 import { z } from "zod"
+import { useCurrentUser } from "../contexts"
 import { users } from "../schema"
 import { userService } from "../services"
 
 export const userResolver = resolver({
+  mine: query(users).resolve(() => useCurrentUser()),
+
   usersByName: query(users.$list())
     .input({ name: z.string() })
     .resolve(({ name }) => userService.findUsersByName(name)),
