c10eff7ba6e8d453898ab976c269374b81ff8870
⛵ <809067559@qq.com>
2025-02-27 01:58:23 +0800

    feat(website): add Lucide icons and update home page layout
    - Add Lucide-Vue-Next library for icon support
    - Improve home page layout, including addition of HomeFeatures component
    - Update styles to prevent horizontal scrolling

--- website/components/home.vue
+++ website/components/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col items-center relative" style="display: flex;">
+  <main class="overflow-x-hidden flex flex-col items-center relative" style="display: flex;">
     <div class="left-0 absolute -z-10 opacity-10 flex flex-col items-center h-[75vh] w-[max(100vh,100vw)]">
       <FlowingLines class="size-full" />
     </div>
@@ -12,10 +12,12 @@
         <slot name="schemaGraphQl"></slot>
       </template>
     </HomeSchemaLibrary>
+    <HomeFeatures />
   </main>
 </template>
 <script lang="tsx" setup>
 import Hero from "./home-hero.vue";
 import FlowingLines from "./flowing-lines.vue";
 import HomeSchemaLibrary from './home-schema-library.vue'
+import HomeFeatures from './home-features.vue'
 </script>
---
+++ website/components/lucide-icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="icon" :class="props.class" />
+</template>
+<script lang="ts" setup>
+import { computed, defineProps } from 'vue'
+import {
+  Fence,
+  HardDriveDownload,
+  RadioTower,
+  Satellite,
+  SatelliteDish,
+  Shuffle,
+} from 'lucide-vue-next'
+
+const icons = {
+  Fence,
+  HardDriveDownload,
+  RadioTower,
+  Satellite,
+  SatelliteDish,
+  Shuffle
+}
+
+export type LucideIconName = keyof typeof icons
+
+const props = defineProps<{
+  icon: LucideIconName
+  class?: string
+}>()
+
+const icon = computed(() => icons[props.icon])
+</script>
