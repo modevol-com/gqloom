5d0460afa946df13561d3ce86458481890c08ebc
⛵ <809067559@qq.com>
2025-03-03 15:42:53 +0800

    feat(website): enhance DocText component styling and layout
    - Add flexbox layout for better alignment
    - Include hover effects for improved interactivity
    - Adjust padding and text color for consistent styling
    - Optimize for better mobile and desktop responsiveness

--- website/components/doc-text/index.tsx
+++ website/components/doc-text/index.tsx
@@ -7,7 +7,10 @@ export default function DocText() {
 
   const text = params.lang === "zh" ? "文档" : "Documentation"
   return (
-    <Link href={`/${params.lang}/docs`} className="font-semibold">
+    <Link
+      href={`/${params.lang}/docs`}
+      className="font-semibold flex flex-row items-center gap-2 rounded-md px-3 py-2.5 text-fd-muted-foreground transition-colors duration-100 [overflow-wrap:anywhere] hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none md:px-2 md:py-1.5 [&_svg]:size-4"
+    >
       {text}
     </Link>
   )
