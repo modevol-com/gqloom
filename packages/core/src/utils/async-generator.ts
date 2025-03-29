5ff44518ba8f76262f08421fc8ab37b3729cc62a
⛵ <809067559@qq.com>
2025-03-29 15:09:17 +0800

    refactor(core): improve bindAsyncGenerator implementation
    - Updated the `bindAsyncGenerator` function to use `apply` for method binding, enhancing flexibility in argument handling.
    - Added type inference for the generator parameter to improve type safety and clarity.

--- packages/core/src/utils/async-generator.ts
+++ packages/core/src/utils/async-generator.ts
@@ -2,24 +2,24 @@ import type { AsyncLocalStorage } from "node:async_hooks"
 
 export function bindAsyncGenerator<
   TAsyncLocalStorage extends AsyncLocalStorage<unknown>,
-  T = unknown,
-  TReturn = any,
-  TNext = unknown,
->(
-  storage: TAsyncLocalStorage,
-  generator: AsyncGenerator<T, TReturn, TNext>
-): AsyncGenerator<T, TReturn, TNext> {
+  TGenerator extends AsyncGenerator<unknown, unknown, unknown>,
+>(storage: TAsyncLocalStorage, generator: TGenerator): TGenerator {
   return {
-    next: storage.run(storage.getStore(), () => generator.next.bind(generator)),
-    return: storage.run(storage.getStore(), () =>
-      generator.return.bind(generator)
-    ),
-    throw: storage.run(storage.getStore(), () =>
-      generator.throw.bind(generator)
-    ),
+    next: (...args) =>
+      storage.run(storage.getStore(), () =>
+        generator.next.apply(generator, args)
+      ),
+    return: (...args) =>
+      storage.run(storage.getStore(), () =>
+        generator.return.apply(generator, args)
+      ),
+    throw: (...args) =>
+      storage.run(storage.getStore(), () =>
+        generator.throw.apply(generator, args)
+      ),
 
     [Symbol.asyncIterator]() {
       return bindAsyncGenerator(storage, generator[Symbol.asyncIterator]())
     },
-  }
+  } as TGenerator
 }
