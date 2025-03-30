716a0a5637d46fcc6795220ad3c699c65c19f304
⛵ <809067559@qq.com>
2025-03-30 23:38:14 +0800

    refactor(core): enhance async generator binding and subscription handling

--- packages/core/src/utils/async-generator.ts
+++ packages/core/src/utils/async-generator.ts
@@ -2,29 +2,43 @@ import type { AsyncLocalStorage } from "node:async_hooks"
 
 export function bindAsyncGenerator<
   TAsyncLocalStorage extends AsyncLocalStorage<unknown>,
-  T = unknown,
-  TReturn = any,
-  TNext = unknown,
->(
-  storage: TAsyncLocalStorage,
-  generator: AsyncGenerator<T, TReturn, TNext>
-): AsyncGenerator<T, TReturn, TNext> {
+  TAsyncIterator extends AsyncIterator<unknown, unknown, unknown>,
+>(storage: TAsyncLocalStorage, generator: TAsyncIterator): TAsyncIterator {
+  if (isAsyncGenerator(generator)) {
+    return {
+      next: (...args) =>
+        storage.run(storage.getStore(), () =>
+          generator.next.apply(generator, args)
+        ),
+      return: (...args) =>
+        storage.run(storage.getStore(), () =>
+          generator.return.apply(generator, args)
+        ),
+      throw: (...args) =>
+        storage.run(storage.getStore(), () =>
+          generator.throw.apply(generator, args)
+        ),
+
+      [Symbol.asyncIterator]() {
+        return bindAsyncGenerator(storage, generator[Symbol.asyncIterator]())
+      },
+    } as TAsyncIterator & AsyncGenerator<unknown, unknown, unknown>
+  }
   return {
     next: (...args) =>
       storage.run(storage.getStore(), () =>
         generator.next.apply(generator, args)
       ),
-    return: (...args) =>
-      storage.run(storage.getStore(), () =>
-        generator.return.apply(generator, args)
-      ),
-    throw: (...args) =>
-      storage.run(storage.getStore(), () =>
-        generator.throw.apply(generator, args)
-      ),
+  } as TAsyncIterator
+}
 
-    [Symbol.asyncIterator]() {
-      return bindAsyncGenerator(storage, generator[Symbol.asyncIterator]())
-    },
-  } satisfies AsyncGenerator<T, TReturn, TNext>
+function isAsyncGenerator<T, TReturn = any, TNext = undefined>(
+  generator: AsyncIterator<T, TReturn, TNext>
+): generator is AsyncGenerator<T, TReturn, TNext> {
+  return (
+    "return" in generator &&
+    typeof generator.return === "function" &&
+    "throw" in generator &&
+    typeof generator.throw === "function"
+  )
 }
