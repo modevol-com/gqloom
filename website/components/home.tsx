84f7161a287677f1e4d2cfc945838f59936d765c
xcfox <xcfox@qq.com>
2025-08-30 03:20:35 +0800

    fix(website): update link to documentation in Home component
    - Changed the href attribute of the documentation link in the Home component from "./docs/guide" to "./docs/guide.html" for correct navigation.

--- website/components/home.tsx
+++ website/components/home.tsx
@@ -311,7 +311,7 @@ const Hero = defineComponent({
               {texts.value.star}
             </a>
             <a
-              href="./docs/guide"
+              href="./docs/guide.html"
               class="no-underline hover:scale-105 ease-out text-nowrap transition duration-300 !text-white px-6 py-3 flex items-center font-medium bg-gradient-to-r to-pink-600 from-orange-400 rounded-full hover:to-pink-500 hover:from-amber-300"
             >
               <span>{texts.value.start}</span>
