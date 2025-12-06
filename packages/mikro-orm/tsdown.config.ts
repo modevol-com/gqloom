2b5d5241b71fe968aad964d73ebf9059745ba7c5
xcfox <xcfox@qq.com>
2025-12-06 18:17:49 +0800

    🐞 fix(mikro-orm): remove context entry

--- packages/mikro-orm/tsdown.config.ts
+++ packages/mikro-orm/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsdown"
 
 export default defineConfig({
-  entry: ["./src/index.ts", "./src/context.ts"],
+  entry: ["./src/index.ts"],
   format: ["esm", "cjs"],
   minify: false,
   dts: true,
